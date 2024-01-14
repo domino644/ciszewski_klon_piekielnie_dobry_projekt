2d167cc202a5293da1ff75731fcfcaf96d72b837
domino644 <cichemleko@gmail.com>
2024-01-14 19:16:52 +0100

    created FontResizer class that helps with adjusting font size to container width so whole text fits

---
+++ ProjekPo/src/main/java/presenter/FontResizer.java
@@ -0,0 +1,20 @@
+package presenter;
+
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+
+public class FontResizer {
+    public static double calculateOptimalFontSize(String text, Font font, double availableWidth){
+        Text helperText = new Text();
+        helperText.setFont(font);
+        helperText.setText(text);
+
+        double textWidth = helperText.getLayoutBounds().getWidth();
+        double fontSize = font.getSize();
+        if (textWidth > availableWidth) {
+            fontSize *= availableWidth / textWidth;
+        }
+
+        return fontSize;
+    }
+}
