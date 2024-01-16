95086c522f9d32d530c781fdb44cf1ab328fcafc
domino644 <cichemleko@gmail.com>
2024-01-16 22:57:18 +0100

    adjusted program to stats saving

--- ProjekPo/src/main/java/model/StatsKeeper.java
+++ ProjekPo/src/main/java/model/StatsKeeper.java
@@ -3,10 +3,7 @@ package model;
 
 import model.animal.Animal;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class StatsKeeper {
     private final WorldMap map;
@@ -87,10 +84,10 @@ public class StatsKeeper {
         int width = upperBound.getX() - lowerBound.getX();
         int height = upperBound.getY() - lowerBound.getY();
 
-        for(int i = 0; i < width; i++){
-            for(int j = 0; j < height; j++){
-                Vector2d v = new Vector2d(i,j);
-                if(!map.isOccupied(v)){
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                Vector2d v = new Vector2d(i, j);
+                if (!map.isOccupied(v)) {
                     numberOfFreeFields++;
                 }
             }
@@ -98,37 +95,37 @@ public class StatsKeeper {
         return numberOfFreeFields;
     }
 
-    public int getNumberOfPlants(){
+    public int getNumberOfPlants() {
         return numberOfPlants;
     }
 
-    public float getAverageLifetime(){
+    public float getAverageLifetime() {
         float avgLifetime = 0;
-        for(Animal animal : deadAnimals){
-            avgLifetime+=animal.getAge();
+        for (Animal animal : deadAnimals) {
+            avgLifetime += animal.getAge();
         }
-        return avgLifetime/deadAnimals.size();
+        return avgLifetime / deadAnimals.size();
     }
 
-    public float getAvgNumberOfKids(){
+    public float getAvgNumberOfKids() {
         float avgNumberOfKids = 0;
-        for(Animal animal : aliveAnimals){
+        for (Animal animal : aliveAnimals) {
             avgNumberOfKids += animal.getNumberOfChildren();
         }
-        return avgNumberOfKids/aliveAnimals.size();
+        return avgNumberOfKids / aliveAnimals.size();
     }
 
-    public List<Vector2d> getMostPopularPlantsFields(){
+    public List<Vector2d> getMostPopularPlantsFields() {
         List<Vector2d> output = new LinkedList<>();
         int biggestNumberOfOccurances = 0;
-        for(Vector2d position : plantsOccurence.keySet()){
-            if(plantsOccurence.get(position) > biggestNumberOfOccurances){
+        for (Vector2d position : plantsOccurence.keySet()) {
+            if (plantsOccurence.get(position) > biggestNumberOfOccurances) {
                 biggestNumberOfOccurances = plantsOccurence.get(position);
             }
         }
 
-        for(Vector2d position : plantsOccurence.keySet()){
-            if(plantsOccurence.get(position) == biggestNumberOfOccurances){
+        for (Vector2d position : plantsOccurence.keySet()) {
+            if (plantsOccurence.get(position) == biggestNumberOfOccurances) {
                 output.add(position);
             }
         }
@@ -136,12 +133,29 @@ public class StatsKeeper {
         return output;
     }
 
-    public int getNumberOfAliveAnimals(){
+    public int getNumberOfAliveAnimals() {
         return aliveAnimals.size();
     }
 
-    public int getNumberOfDeadAnimals(){
+    public int getNumberOfDeadAnimals() {
         return deadAnimals.size();
+    }
+
+    public String[] getDailtyStatsAsString() {
+        Genotype genotype = getMostPopularGenotype();
+        String g;
+        if(Objects.isNull(genotype)){
+            g = "null";
+        }else{
+            g = genotype.toString();
+        }
+        return new String[]{String.valueOf(getNumberOfAliveAnimals()),
+                String.valueOf(getNumberOfPlants()),
+                String.valueOf(getNumberOfFreeFields()),
+                String.valueOf(getAverageEnergyLevel()),
+                String.valueOf(getAverageLifetime()),
+                String.valueOf(getAvgNumberOfKids()),
+                g};
     }
 
 }
--- ProjekPo/src/main/java/presenter/StartPresenter.java
+++ ProjekPo/src/main/java/presenter/StartPresenter.java
@@ -3,6 +3,7 @@ package presenter;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Spinner;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
@@ -13,6 +14,7 @@ import model.records.AllParameters;
 import model.simulation.Simulation;
 import model.simulation.SimulationInitialize;
 import presenter.components.WorldElementBox;
+import presenter.utils.CSVHandler;
 import presenter.utils.JSONHandler;
 
 import java.io.IOException;
@@ -46,55 +48,68 @@ public class StartPresenter {
     private Spinner<Integer> widthSpinner;
     @FXML
     private TextField fileNameTextField;
+    @FXML
+    private TextField statsNameTextField;
+    @FXML
+    private CheckBox saveStatsCheckBox;
     private int simulationCount = 1;
 
     public void simulationStart() {
-        FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(getClass().getClassLoader().getResource("simulation.fxml"));
-        BorderPane viewRoot;
-        Scene scene;
-        try {
-            viewRoot = loader.load();
-            scene = new Scene(viewRoot);
-        } catch (IOException e) {
-            System.out.println("Couldn't load file");
-            return;
+        if (!(saveStatsCheckBox.isSelected() && statsNameTextField.getText().trim().isEmpty())) {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getClassLoader().getResource("simulation.fxml"));
+            BorderPane viewRoot;
+            Scene scene;
+            try {
+                viewRoot = loader.load();
+                scene = new Scene(viewRoot);
+            } catch (IOException e) {
+                System.out.println("Couldn't load file");
+                return;
+            }
+            SimulationPresenter presenter = loader.getController();
+            Stage stage = new Stage();
+            stage.setScene(scene);
+            stage.setTitle("Darwin world # " + simulationCount);
+            stage.minWidthProperty().bind(viewRoot.minWidthProperty());
+            stage.minHeightProperty().bind(viewRoot.minHeightProperty());
+            SimulationInitialize simulationInitialize = new SimulationInitialize(widthSpinner.getValue(),
+                    heightSpinner.getValue(),
+                    numberOfAnimalsSpinner.getValue(),
+                    numberOfPlantsSpinner.getValue(),
+                    numberOfPlantsGrowPerDaySpinner.getValue(),
+                    startAnimalEnergySpinner.getValue(),
+                    minimalEnergyToReproductionSpinner.getValue(),
+                    reproducingEnergySpinner.getValue(),
+                    minimalMutationNumberSpinner.getValue(),
+                    maximalMutationNumberSpinner.getValue(),
+                    genomeLengthSpinner.getValue(),
+                    plantEnergySpinner.getValue(),
+                    lostEnergyPerDaySpinner.getValue(),
+                    saveStatsCheckBox.isSelected(),
+                    statsNameTextField.getText());
+            WorldMap map = simulationInitialize.getWorldMap();
+            WorldElementBox worldElementBox = new WorldElementBox(startAnimalEnergySpinner.getValue(), map);
+            Simulation simulation = simulationInitialize.getSimulation();
+            simulationCount++;
+            presenter.setWorldElementBox(worldElementBox);
+            presenter.setWorldMap(map);
+            presenter.setWorldSimulation(simulation);
+            map.addListener(presenter);
+            stage.setOnCloseRequest(event -> handleCloseRequest(event, simulation));
+            stage.show();
+        }else{
+            System.out.println("Podaj nazwe pliku do zapisu statystyk");
         }
-        SimulationPresenter presenter = loader.getController();
-        Stage stage = new Stage();
-        stage.setScene(scene);
-        stage.setTitle("Darwin world # " + simulationCount);
-        stage.minWidthProperty().bind(viewRoot.minWidthProperty());
-        stage.minHeightProperty().bind(viewRoot.minHeightProperty());
-        SimulationInitialize simulationInitialize = new SimulationInitialize(widthSpinner.getValue(),
-                heightSpinner.getValue(),
-                numberOfAnimalsSpinner.getValue(),
-                numberOfPlantsSpinner.getValue(),
-                numberOfPlantsGrowPerDaySpinner.getValue(),
-                startAnimalEnergySpinner.getValue(),
-                minimalEnergyToReproductionSpinner.getValue(),
-                reproducingEnergySpinner.getValue(),
-                minimalMutationNumberSpinner.getValue(),
-                maximalMutationNumberSpinner.getValue(),
-                genomeLengthSpinner.getValue(),
-                plantEnergySpinner.getValue(),
-                lostEnergyPerDaySpinner.getValue());
-        WorldMap map = simulationInitialize.getWorldMap();
-        WorldElementBox worldElementBox = new WorldElementBox(startAnimalEnergySpinner.getValue(), map);
-        Simulation simulation = simulationInitialize.getSimulation();
-        simulationCount++;
-        presenter.setWorldElementBox(worldElementBox);
-        presenter.setWorldMap(map);
-        presenter.setWorldSimulation(simulation);
-        map.addListener(presenter);
-        stage.setOnCloseRequest(event -> handleCloseRequest(event, simulation));
-        stage.show();
     }
 
     private void handleCloseRequest(WindowEvent event, Simulation simulation) {
         System.out.println("Close Simulation");
         simulation.setSimulationPlay(false);
         simulation.setKillSimulation(true);
+        if(simulation.saveStats){
+            CSVHandler.writeStatsToCSV(simulation.stats,simulation.fileName);
+        }
     }
 
     @FXML
@@ -112,7 +127,9 @@ public class StartPresenter {
                     maximalMutationNumberSpinner.getValue(),
                     genomeLengthSpinner.getValue(),
                     plantEnergySpinner.getValue(),
-                    lostEnergyPerDaySpinner.getValue());
+                    lostEnergyPerDaySpinner.getValue(),
+                    false,
+                    "");
             AllParameters params = simulationInitialize.getAllParameters();
             JSONHandler.objectToFile(params, fileNameTextField.getText().trim());
         }
@@ -136,7 +153,7 @@ public class StartPresenter {
                 genomeLengthSpinner.getValueFactory().setValue(allParameters.worldParameters().genomeLength());
                 plantEnergySpinner.getValueFactory().setValue(allParameters.simulationParameters().plantEnergy());
                 lostEnergyPerDaySpinner.getValueFactory().setValue(allParameters.simulationParameters().lostEnergyPerDay());
-    }
+            }
         }
     }
 
